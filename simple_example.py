ee96d2a510430ac58efcc640b815a644b7213dab
Kim Borgen <me@kimborgen.com>
2019-03-13 15:56:37 +0100

    Added a simple example of how to get data from CREPE

--- simple_example.py
+++ simple_example.py
@@ -2,31 +2,30 @@ from CREPE import CREPE
 from CREPE.utils import get_connection
 from CREPE.communication.stream_service import StreamRowIterator, StreamSegmentIterator
 
+import time
 import os,sys,inspect 
+
 # Find the path to the test_data folder.
 __currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 path_to_test_data_folder = __currentdir + "/test_data/"
 
-
-# Feel free to change this method, currently it is for testing purposes
+# A simple way to get data
 def main():
     # Get the absolute path for the test file
     path_to_data = path_to_test_data_folder + "4.h5"
 
     #Create a crepe object and start it
-    crep = CREPE("__TESTING")
+    crep = CREPE(path_to_data)
          
     # Get the connection for the stream whichs contains the data
     conn = get_connection("STREAM")
     
     # Get a row iterator
-    row_iter = StreamSegmentIterator(_range = 100)
+    row_iter = StreamRowIterator(_channel = 0, _range = 100)
     
     # Our simple experiment is just to count how many data points we have in a channel :)
     len_of_stream = 0
     
-    print("Starting to iterate")
-
     # Now we want to iterate 
     while True:
         # Get the next set of data. 
@@ -37,7 +36,7 @@ def main():
             break
 
         # Add the length of this data
-        len_of_stream += len(row[0])
+        len_of_stream += len(row)
 
     print("The length from the stream was: ", len_of_stream)
     
