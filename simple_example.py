41985222426e965b96e169c8dc95ce8b99c87af6
Kim Borgen <me@kimborgen.com>
2019-04-03 16:40:47 +0200

    expand simple example with more examples

--- simple_example.py
+++ simple_example.py
@@ -1,5 +1,6 @@
 from CREPE import CREPE, CrepeModus
 from CREPE.utils.get_queue import get_queue 
+from CREPE.communication.queue_service import is_poison_pill, QueueService
 
 import time
 import os,sys,inspect 
@@ -14,19 +15,26 @@ def main():
     file_path = path_to_test_data_folder + "4.h5"
 
     #Create a crepe object and start it
-    crep = CREPE(modus=CrepeModus.FILE, file_path=file_path)
+    crep = CREPE(modus=CrepeModus.FILE, file_path=file_path)    
     
+    #get_data_from_meame_direct_from_queue(crep)
+    get_data_from_meame_with_helper(crep)
+    #get_data_from_meame_with_easier_helper(crep)
+
+def get_data_from_meame_direct_from_queue(crep):
     # Get the hdf5 queue
-    queue = get_queue(crep, "HDF5READER")
+    queue = crep.get_first_queue()
         
     # Our simple experiment is just to count how many data points we have in a channel :)
     len_of_stream = 0
-    
+     
     # Now we want to iterate 
     while True:
         try: 
             # get the next element in the queue
             data = queue.get(timeout=1) #1 seconds timeout
+            if is_poison_pill(data):
+                break
         except: # .get raises error on timeout
             break
         # Add the length of this data
@@ -36,6 +44,23 @@ def main():
     
     # Shutdown crepe
     crep.shutdown()
+
+def get_data_from_meame_with_helper(crep):
+    helper = QueueService(name="HELPER", queue_in=crep.get_first_queue())
+    i = 0
+    while True:
+        data = helper.get()
+        if data is False:
+            crep.shutdown()
+            break
+        i += len(data[0])
+    print("Got ", i, " elements in each row")
+
+def get_data_from_meame_with_easier_helper(crep):
+    def data_func(data):
+        #do something with data, for example print it
+        print(data)
+    crep.wait(data_func)
 
 if __name__ == "__main__":
     main()
